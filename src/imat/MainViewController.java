42511fe4c15b0dcc86090bd24c905633a8aa7024
EmmaDahlqvist <emma.dahlqvist@elev.ga.ntig.se>
2024-05-03 09:17:58 +0200

    init

---
+++ src/imat/MainViewController.java
@@ -0,0 +1,34 @@
+
+package imat;
+
+import java.net.URL;
+import java.time.LocalDateTime;
+import java.util.ResourceBundle;
+
+import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
+import javafx.scene.control.Label;
+import se.chalmers.cse.dat216.project.IMatDataHandler;
+import se.chalmers.cse.dat216.project.Product;
+
+public class MainViewController implements Initializable {
+
+    @FXML
+    Label pathLabel;
+
+    IMatDataHandler iMatDataHandler = IMatDataHandler.getInstance();
+
+    public void initialize(URL url, ResourceBundle rb) {
+
+        String iMatDirectory = iMatDataHandler.imatDirectory();
+
+        pathLabel.setText(iMatDirectory);
+
+        for(Product product : iMatDataHandler.getProducts()) {
+            System.out.println(product);
+
+        }
+
+    }
+
+}
